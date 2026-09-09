ba25b44d22cf2385d507454fdb92567cd206f7fa
CIELMATT <rahhhh2000@gmail.com>
2026-09-08 21:23:09 -0400

    Initialize Easter class with main method
    Added a skeleton for the Easter class with a main method.

--- Easter.java
+++ Easter.java
@@ -1,1 +1,10 @@
+// Your Name
+// Date: 9/8/2026
+// This program will calculate the date Easter falls on given a year after 1583.
+import java.util.*;
 
+public class Easter {
+    public static void main(String[] args) {
+        
+    }
+}
